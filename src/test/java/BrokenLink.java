1689c107a5580dcfa75cff3ba4a08404db7285e3
Shivapriya <90460346+Shivapriya1726@users.noreply.github.com>
2024-04-12 19:38:31 +0530

    Update BrokenLink.java

--- src/test/java/BrokenLink.java
+++ src/test/java/BrokenLink.java
@@ -2,32 +2,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.IOException;
 
-public class BrokenLink {
+public class BrokenLinkChecker {
     public static void main(String[] args) {
-        String siteHost = "https://www.wikipedia.org/"; // Replace URL
-        List<String> urls = new ArrayList<>(); // URLs to check
+        String siteHost = "https://wikipedia.org"; 
+        List<String> urls = new ArrayList<>(); 
         urls.add(siteHost);
 
         List<String> brokenLinks = new ArrayList<>();
 
-        int pointer = 0;
+        int pointer = 0; 
 
-        // Set up Selenium WebDriver
         System.setProperty("webdriver.chrome.driver", "D://chromedriver-win64//chromedriver-win64//chromedriver.exe/");
         WebDriver driver = new ChromeDriver();
 
-
         while (urls.size() > pointer) {
-
+            // Step 2: Retrieve one URL from the “to check list”
             String url = urls.get(pointer);
             System.out.println("URL: " + url);
-
 
             boolean flagBroken = isBrokenLink(url);
             pointer += 1;
@@ -36,14 +34,12 @@ public class BrokenLink {
                 continue;
             }
 
-
             driver.get(url);
             try {
                 Thread.sleep(2000); // allow JS to load
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
 
             List<WebElement> linkElements = driver.findElements(By.tagName("a"));
             System.out.println("New Links count: " + linkElements.size());
@@ -57,7 +53,6 @@ public class BrokenLink {
             }
         }
 
-
         System.out.println("Total Page Count: " + urls.size());
         System.out.println("Total Broken Links: " + brokenLinks.size());
         for (String brokenLink : brokenLinks) {
@@ -68,7 +63,6 @@ public class BrokenLink {
         driver.quit();
     }
 
-    // Method to check if a link is broken
     private static boolean isBrokenLink(String url) {
         HttpURLConnection connection = null;
         try {
@@ -79,7 +73,7 @@ public class BrokenLink {
             int responseCode = connection.getResponseCode();
             return responseCode != HttpURLConnection.HTTP_OK;
         } catch (IOException e) {
-            // If request fails, treat it as broken
+      
             return true;
         } finally {
             if (connection != null) {
